61d0637626dca1c2d20708e3b7d9b2c13c26b8f3
Nick Olszanski <nick@dinosaurs-with-jetpacks.club>
2021-12-25 23:13:17 +0100

    cleanup

--- src/stores/_root.store.ts
+++ src/stores/_root.store.ts
@@ -10,83 +10,31 @@ import {
 // don't allow state modifications outside actions
 configure({ enforceActions: "always" })
 
-// type EditotContainers = AuthContainer | A_Container;``
-type ContainerState = "pristine" | "fetching" | "fetched"
-
-const Symbs = {
-  pizzaContainer: "pizzaContainerSymbol",
-}
-
-// interface ContainerCache {
-//   state
-//   container: Object
-// }
-
-type ContainerCache<ContainerType> =
-  | {
-      state: "fetching"
-      containerPromise: Promise<ContainerType>
-    }
-  | {
-      state: "fetched"
-      containerPromise: Promise<ContainerType>
-      container: ContainerType
-    }
-
 interface ContainerRegistry {
   auth: Promise<AuthContainer>
   pizzaContainerSymbol: Promise<PizzaPlace_Container>
 }
 
-interface BetterContainerRegistry {
-  // authConttainer?: ContainerCache<AuthContainer>
-  pizzaContainerSymbol?: ContainerCache<PizzaPlace_Container>
-}
 type ValueOf<T> = T[keyof T]
 export class RootContainer {
   private authContainer?: AuthContainer
   private a?: A_Container
   private b?: B_Container
-  private pizzaPlace?: PizzaPlace_Container
 
-  private fetchingStates: { [key: string]: ContainerState } = {}
   private containerCache: Partial<ContainerRegistry> = {}
-
-  // private betterContainerCache: BetterContainerRegistry = {}
 
   public async getPizzaPlaceContainer2(): Promise<PizzaPlace_Container> {
     const key = "pizzaContainerSymbol" as const
     const containerProvider = providePizzaPlaceContainer
     const z = await this.getGenericContainer(key, containerProvider)
 
-    return
+    return z
   }
 
-  public async getGenericContainer(
+  public async getGenericContainer<T extends ValueOf<ContainerRegistry>>(
     key: keyof ContainerRegistry,
-    containerProvider: () => ValueOf<ContainerRegistry>,
-    // ): Promise<ContainerInstance> {
-  ): Promise<ValueOf<ContainerRegistry>> {
-    // let k: ValueOf<ContainerRegistry>
-    // console.log(k)
-    // if (this.betterContainerCache[key] == null) {
-    //   const containerPromise = containerProvider()
-
-    //   this.betterContainerCache[key] = {
-    //     state: "fetching",
-    //     containerPromise: containerPromise,
-    //   }
-    // }
-
-    // if (this.fetchingStates[key] == null) {
-    //   this.fetchingStates[key] = "fetching"
-    //   const containerPromise = containerProvider()
-    //   this.containerCache[key] = containerPromise as any
-    //   await containerPromise
-    //   this.fetchingStates[key] = "fetched"
-    //   return containerPromise
-    // }
-
+    containerProvider: () => T,
+  ): Promise<T> {
     if (this.containerCache[key] == null) {
       const containerPromise = containerProvider()
       this.containerCache[key] = containerPromise as any
@@ -102,53 +50,6 @@ export class RootContainer {
       }
     }
 
-    // if (
-    //   this.fetchingStates[key] === "fetching" &&
-    //   this.containerCache[key] != null
-    // ) {
-    //   const containerPromise = this.containerCache[key]
-    //   const cache = await containerPromise
-    //   if (cache != null) {
-    //     return cache
-    //   }
-    // }
-    // if (this.fetchingStates[key] === "fetched") {
-    //   const cache = await this.containerCache[key]
-    //   if (cache != null) {
-    //     return cache
-    //   }
-    // }
-    throw new Error("WTF")
-  }
-
-  public async getPizzaPlaceContainer(): Promise<PizzaPlace_Container> {
-    const key = "pizzaContainerSymbol" as const
-    const containerProvider = providePizzaPlaceContainer
-
-    if (this.fetchingStates[key] == null) {
-      this.fetchingStates[key] = "pristine"
-    }
-
-    if (this.fetchingStates[key] === "pristine") {
-      this.fetchingStates[key] = "fetching"
-      const containerPromise = containerProvider()
-      this.containerCache[key] = containerPromise
-      this.pizzaPlace = await containerPromise
-      this.fetchingStates[key] = "fetched"
-      return this.pizzaPlace
-    }
-    if (this.fetchingStates[key] === "fetching") {
-      const cache = await this.containerCache[key]
-      if (cache != null) {
-        return cache
-      }
-    }
-    if (this.fetchingStates[key] === "fetched") {
-      const cache = await this.containerCache[key]
-      if (cache != null) {
-        return cache
-      }
-    }
     throw new Error("WTF")
   }
 
--- src/stores/container.pizza-place.hook.ts
+++ src/stores/container.pizza-place.hook.ts
@@ -4,5 +4,5 @@ import type { PizzaPlace_Container } from "./container.pizza-place"
 
 export function usePizzaPlaceContainer(): ContainerGeneric<PizzaPlace_Container> {
   const root = useRootStore()
-  return useGenericContainer(root.getPizzaPlaceContainer())
+  return useGenericContainer(root.getPizzaPlaceContainer2())
 }
