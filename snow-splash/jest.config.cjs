177ee7ab6760b412c5a0bd30b542c1187ab5f1ac
Nick Olszanski <nick@dinosaurs-with-jetpacks.club>
2022-02-01 00:05:07 +0100

    adds jest config / setup

---
+++ snow-splash/jest.config.cjs
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/** @type {import('@types/jest')} */
+module.exports = {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "jsdom",
+}
