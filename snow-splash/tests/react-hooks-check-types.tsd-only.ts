8392f4f01c025e8e9dd4f8cfd87e466bcacf1ce2
Nick Olszanski <nick@dinosaurs-with-jetpacks.club>
2022-02-01 00:54:26 +0100

    Add jest and type tests (#3)
    * adds jest config / setup
    * update tsd type checking
    * update core and tests
    * adds gh action

---
+++ snow-splash/tests/react-hooks-check-types.tsd-only.ts
@@ -0,0 +1,39 @@
+import { expectType, expectNotType } from "tsd"
+import {
+  useMockAppContainer,
+  useMockAppContainerSet,
+} from "./mocks/_mock-app-hooks"
+import { A_Container } from "./mocks/container.a"
+import { B_Container } from "./mocks/container.b"
+import { C_Container } from "./mocks/container.c"
+import { MockAppWrapper } from "./mocks/_mock-app-components"
+
+// useMockAppContainer should not return `any` type"
+;(() => {
+  const containers = useMockAppContainer()
+  expectNotType<any>(containers)
+})()
+
+// useMockAppContainer should test if useMockAppContainer gets correct types
+;(() => {
+  const [aContainer] = useMockAppContainer().aCont
+  expectType<undefined | A_Container>(aContainer)
+  if (aContainer != null) {
+    expectType<A_Container>(aContainer)
+  }
+})()
+
+// useMockAppContainerSet should not retun any
+;(() => {
+  const containerSet = useMockAppContainerSet(["aCont", "bCont"])
+  expectNotType<any>(containerSet)
+})()
+
+// useMockAppContainerSet should return exact types
+;(() => {
+  const containerSet = useMockAppContainerSet(["aCont", "bCont"])
+  if (containerSet != null) {
+    expectType<A_Container>(containerSet.aCont)
+    // expectType<undefined>(containerSet.cCont)
+  }
+})()
