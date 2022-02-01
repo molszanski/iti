fcf376e5153d9cd9bd68ea73e5965008d53a4113
Nick Olszanski <nick@dinosaurs-with-jetpacks.club>
2022-02-01 12:42:57 +0100

    removes `containerSetNew` hook

--- snow-splash/tests/mocks/_mock-app-hooks.ts
+++ snow-splash/tests/mocks/_mock-app-hooks.ts
@@ -6,7 +6,6 @@ export const MyRootCont = React.createContext(<MockAppContainer>{})
 
 let mega = getContainerSetHooks(getProviders, MyRootCont)
 export const useMockAppContainerSet = mega.useContainerSet
-export const useMockAppContainerSetNew = mega.useContainerSetNew
 export function useMockAppContainer() {
   const root = useContext(MyRootCont)
   return mega.useRootContainerMap(root.providerMap, root)
