9e2045d77948daab80d4741c93d1cbc164f87cd5
Nick Olszanski <nick@dinosaurs-with-jetpacks.club>
2022-02-01 01:50:39 +0100

    refactors hooks

--- snow-splash/src/react/library.hooks.ts
+++ snow-splash/src/react/library.hooks.ts
@@ -17,28 +17,19 @@ export type ContainerGenericBettter<T> = [
 
 export function useBetterGenericContainer<T>(
   containerPromise: () => Promise<T>,
-  controls: {
-    onContainerUpdate(cb: (container: T) => void): void
-    unsubscribeFunction?: () => void
-    subscribeFunction?(cb: (container: T) => void): () => void
-    containerKey: string
-  },
+
+  subscribeFunction: (cb: (container: T) => void) => () => void,
+  containerKey: string,
 ): ContainerGenericBettter<T> {
   const [data, setData] = useState<any>(undefined)
   const [error, setError] = useState()
 
   // Update container
   useEffect(() => {
-    if (controls.onContainerUpdate) {
-      controls.onContainerUpdate((container) => {
-        setData(container)
-      })
-      return controls.unsubscribeFunction
+    if (subscribeFunction) {
+      return subscribeFunction((cont) => setData(cont))
     }
-    if (controls.subscribeFunction) {
-      return controls.subscribeFunction((cont) => setData(cont))
-    }
-  }, [controls])
+  }, [subscribeFunction])
 
   // We can add optimizations later.
   useEffect(() => {
@@ -49,5 +40,5 @@ export function useBetterGenericContainer<T>(
       .catch((e) => setError(e))
   }, [])
 
-  return [data, error, controls.containerKey]
+  return [data, error, containerKey]
 }
