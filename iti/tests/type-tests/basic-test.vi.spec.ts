25adbdd80d0ff99625bb7c1495db06f73e76ffbc
Nick Olszanski <nick@dinosaurs-with-jetpacks.club>
2025-08-19 02:28:33 +0200

    better type tests

---
+++ iti/tests/type-tests/basic-test.vi.spec.ts
@@ -0,0 +1,31 @@
+import { attest } from "@ark/attest"
+import { describe, it, expect } from "vitest"
+import { createContainer } from "../../src/iti"
+import dedent from "dedent"
+
+import { getMainMockAppContainer } from "../mocks/_mock-app-container"
+import type { A_Container } from "../mocks/container.a"
+import type { B_Container } from "../mocks/container.b"
+import type { C_Container } from "../mocks/container.c"
+
+it("should check container types", async () => {
+  const cont = getMainMockAppContainer()
+
+  const aStuff = await cont.items.aCont
+
+  attest<A_Container>(await cont.items.aCont)
+  attest<B_Container>(await cont.get("bCont"))
+  attest<Promise<C_Container>>(cont.items.cCont)
+})
+
+it("should check token types", () => {
+  const cont = getMainMockAppContainer()
+  attest<{ aCont: "aCont"; bCont: "bCont"; cCont: "cCont" }>(cont.getTokens())
+})
+
+it("should check getContainerSet types", async () => {
+  const cont = getMainMockAppContainer()
+  let itemSet = await cont.getContainerSet(["aCont", "bCont"])
+  attest<A_Container>(itemSet.aCont)
+  attest<B_Container>(itemSet.bCont)
+})
