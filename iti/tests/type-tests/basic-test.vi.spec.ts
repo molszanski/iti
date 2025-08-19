c8a19dcc2115da507b1195b4ca07b113505b7b12
Nick Olszanski <nick@dinosaurs-with-jetpacks.club>
2025-08-19 11:56:00 +0200

    fix[ext]: yolo-3

--- iti/tests/type-tests/basic-test.vi.spec.ts
+++ iti/tests/type-tests/basic-test.vi.spec.ts
@@ -1,5 +1,5 @@
 import { attest } from "@ark/attest"
-import { describe, it, expect } from "vitest"
+import { describe, it, expect, beforeEach } from "vitest"
 import { createContainer } from "../../src/iti"
 import dedent from "dedent"
 
@@ -8,43 +8,62 @@ import type { A_Container } from "../mocks/container.a"
 import type { B_Container } from "../mocks/container.b"
 import type { C_Container } from "../mocks/container.c"
 
-it("should check container types", async () => {
-  const cont = getMainMockAppContainer()
+type MockTokens = {
+  aCont: "aCont"
+  bCont: "bCont"
+  cCont: "cCont"
+}
 
-  attest<A_Container>(await cont.items.aCont)
-  attest<B_Container>(await cont.get("bCont"))
-  attest<Promise<C_Container>>(cont.items.cCont)
-  attest.instantiations([2860, "instantiations"])
-})
+describe("Type tests:", () => {
+  // let cont = getMainMockAppContainer()
+  // beforeEach(() => (cont = getMainMockAppContainer()))
 
-it("should check token types", () => {
-  const cont = getMainMockAppContainer()
-  attest<{ aCont: "aCont"; bCont: "bCont"; cCont: "cCont" }>(cont.getTokens())
-  attest.instantiations([2605, "instantiations"])
-})
+  it("should check container types", async () => {
+    const cont = getMainMockAppContainer()
 
-it("should check getContainerSet types", async () => {
-  const cont = getMainMockAppContainer()
-  let itemSet = await cont.getContainerSet(["aCont", "bCont"])
+    attest<A_Container>(await cont.items.aCont)
+    attest<B_Container>(await cont.get("bCont"))
+    attest<Promise<C_Container>>(cont.items.cCont)
 
-  attest<A_Container>(itemSet.aCont)
-  attest<B_Container>(itemSet.bCont)
-  attest<{
-    aCont: A_Container
-    bCont: B_Container
-  }>(itemSet)
-  attest.instantiations([2768, "instantiations"])
-})
+    attest.instantiations([2860, "instantiations"])
+  })
 
-it("should check getContainerSet function types", async () => {
-  const cont = getMainMockAppContainer()
-  let itemSet = await cont.getContainerSet((c) => [c.aCont, c.bCont])
-  attest<A_Container>(itemSet.aCont)
-  attest<B_Container>(itemSet.bCont)
-  attest<{
-    aCont: A_Container
-    bCont: B_Container
-  }>(itemSet)
+  it("should check token types", () => {
+    attest<MockTokens>(getMainMockAppContainer().getTokens())
+    attest.instantiations([2605, "instantiations"])
+  })
 
-  attest.instantiations([2811, "instantiations"])
+  it("should check getContainerSet types", async () => {
+    const cont = getMainMockAppContainer()
+    const itemSet = await cont.getContainerSet(["aCont", "bCont"])
+
+    attest<A_Container>(itemSet.aCont)
+    attest<B_Container>(itemSet.bCont)
+    attest<{
+      aCont: A_Container
+      bCont: B_Container
+    }>(itemSet)
+    attest.instantiations([2768, "instantiations"])
+  })
+
+  it("should check getContainerSet function types", async () => {
+    const cont = getMainMockAppContainer()
+    const itemSet = await cont.getContainerSet((c) => [c.aCont, c.bCont])
+    attest<A_Container>(itemSet.aCont)
+    attest<B_Container>(itemSet.bCont)
+    attest<{
+      aCont: A_Container
+      bCont: B_Container
+    }>(itemSet)
+
+    attest.instantiations([2811, "instantiations"])
+  })
+
+  it("should check getContainerSet cb has proper tokens", () => {
+    getMainMockAppContainer().getContainerSet((c) => {
+      attest<MockTokens>(c)
+      return [c.aCont]
+    })
+    attest.instantiations([2692, "instantiations"])
+  })
 })
