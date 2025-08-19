a5f4b58ba79f7ed43573410d6007600528feb7f2
Nick Olszanski <nick@dinosaurs-with-jetpacks.club>
2025-08-19 02:18:01 +0200

    fix[ext]: yolo-2

--- iti/tests/type-tests/init.vi.spec.ts
+++ iti/tests/type-tests/init.vi.spec.ts
@@ -2,7 +2,7 @@ import { attest } from "@ark/attest"
 import { describe, it, expect } from "vitest"
 import { type } from "arktype"
 import { createContainer } from "../../src/iti"
-
+import dedent from "dedent"
 enum UniqueResult {
   A,
   B,
@@ -12,19 +12,6 @@ enum UniqueResult {
 
 // @ark/attest assertions can be made from any unit test framework with a global setup/teardown
 describe("attest features", () => {
-  it("type and value assertions", () => {
-    // const even = type("number%2")
-    // asserts even.infer is exactly number
-    let a = 12
-    attest<number>(a)
-    // make assertions about types and values seamlessly
-    // attest(even.infer).type.toString.snap("number")
-    // // including object literals- no more long inline strings!
-    // attest(even.json).snap({
-    //   intersection: [{ domain: "number" }, { divisor: 2 }],
-    // })
-  })
-
   // results produced by an add should valid
   it("should check getter types", () => {
     const node = createContainer()
@@ -40,26 +27,14 @@ describe("attest features", () => {
     attest<UniqueResult.B>(node.get("b"))
     attest<UniqueResult.C>(node.items.c)
 
-    attest(node).type.toString.snap(`Container<
-  {
-    a: UniqueResult
-    b: () => UniqueResult.B
-    c: () => UniqueResult.C
-  },
-  {}
->`)
-
-    //     attest(node).type.toString.snap(`Container<
-    //   {
-    //     a: UniqueResult
-    //     b: () => UniqueResult.B
-    //     c: () => UniqueResult.C
-    //   },
-    //   {}
-    // >
-    // `)
-
-    // expectNotType<any>(node)
+    attest(node).type.toString.snap(dedent`Container<
+      {
+        a: UniqueResult
+        b: () => UniqueResult.B
+        c: () => UniqueResult.C
+      },
+      {}
+    >`)
   })
 })
 // describe("init", () => {
